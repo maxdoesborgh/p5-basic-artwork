1ffc9b396e844d7a90b00b15444ed6c4887482a0
Max Doesborgh <max.doesborgh1983@gmail.com>
2026-03-15 07:37:54 +0100

    Readme template added changedrm -rf .git

--- sketch.js
+++ sketch.js
@@ -1,8 +1,21 @@
 function setup() {
+  // Dit maakt het canvas zo groot als je browserscherm
   createCanvas(windowWidth, windowHeight);
+  background(20);
 }
 
 function draw() {
-  background(220);
-  ellipse(mouseX, mouseY, 50, 50); // Een cirkel die je muis volgt
+  // Een hele lichte transparante achtergrond zorgt voor "trails" (sporen)
+  background(20, 10); 
+  
+  // Teken een cirkel op de plek van je muis
+  noStroke();
+  fill(0, 255, 200);
+  ellipse(mouseX, mouseY, 50, 50);
+}
+
+// Zorgt dat het kunstwerk niet kapot gaat als je je scherm herschaalt
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  background(20);
 }
